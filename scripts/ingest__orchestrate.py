abe40df511f14c3216c1e9c451e696945be74be4
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-07-09 09:31:44 -0400

    Import logging

--- scripts/ingest__orchestrate.py
+++ scripts/ingest__orchestrate.py
@@ -1,6 +1,13 @@
 from utils.bigquery.create_datasets import create_datasets
+import logging
 
 def main():
+
+    # set logging config
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s"
+    )
 
     # create datasets
     create_datasets()
