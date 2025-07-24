d038673da19434f7c0ea625081b81945e5365be6
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-07-24 16:02:47 -0400

    Retrieve list of player urls

---
+++ scripts/web/tennisabstract/players/create_player_url.py
@@ -0,0 +1,23 @@
+from typing import (
+    Dict,
+)
+
+def create_player_url(
+    player_name: str,
+    player_gender: str
+) -> str:
+    """
+    Arguments:
+    - player_name: Player name
+    - player_gender: Player gender
+
+    Returns player url
+    """
+
+    # format parts of url string
+    url_player_str = 'player' if player_gender == 'M' else 'wplayer'
+    url_player_name = player_name.replace(' ', '')
+
+    player_url = f"https://www.tennisabstract.com/cgi-bin/{url_player_str}.cgi?p={url_player_name}"
+
+    return player_url
