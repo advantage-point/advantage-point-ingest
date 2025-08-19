11e7875de10acaee1d32113aa7c925b6b650e7cc
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-08-19 00:42:15 -0400

    Add player_classic scripts

--- scripts/web/tennisabstract/players_classic/get_player_classic_data_scraped.py
+++ scripts/web/tennisabstract/players_classic/get_player_classic_data_scraped.py
@@ -10,7 +10,6 @@ from utils.web.scrape_javascript_var import (
     scrape_javascript_var,
 )
 import ast
-import json
 import logging
 import time
 
@@ -65,7 +64,6 @@ def get_player_classic_data_scraped(
                             if val is not None:
                                 val = ast.literal_eval(val)
                                 val = parse_player_classic_matchmx(player_matchmx_list=val)
-                                val = json.dumps(val)
                         except Exception as e:
                             logging.error(f"Error scraping {var}: {e}.")
                             
@@ -75,10 +73,6 @@ def get_player_classic_data_scraped(
                             content=response_text,
                             var=var
                         )
-
-                        # convert to string: add extra quotes around the value if not exists
-                        if (val is not None) and ("'" not in val) and ('"' not in val) :
-                            val = f"'{val}'"
                         
                     player_dict[var] = val
                 except Exception as e:
@@ -94,116 +88,6 @@ def get_player_classic_data_scraped(
                 missing_vars = [var for var, val in player_dict.items() if val is None]
                 logging.info(f"Missing variables: {missing_vars}")
 
-                attempt += 1
-                
-                # retry if possible
-                if attempt < retries:
-                    logging.info(f"Retrying in {delay} seconds...Attempt {attempt} for {player_classic_url}")
-                    time.sleep(delay)
-                    continue
-                else:
-                    logging.info(f"Max retries reached for {player_classic_url}...Returning empty dictionary")
-                return {}
-
-            # return dictionary if data successfully extracted
-            return player_dict
-
-        except Exception as e:
-            attempt += 1
-            logging.warning(f"Attempt {attempt} failed for {player_classic_url}: {e}")
-
-            if attempt < retries:
-                logging.info(f"Retrying in {delay} seconds...")
-                time.sleep(delay)  # Delay before retrying
-            else:
-                logging.info(f"Max retries reached for {player_classic_url}.")
-
-    # Return empty dictionary if all retries fail
-    logging.info(f"Returning empty dictionary")
-    return {}
-
-def get_player_classic_data_scraped_test(
-    player_classic_url: str,
-    retries: int = 3,
-    delay: int = 3
-) -> Dict:
-    """
-    Arguments:
-    - player_classic_url: player classic link
-    - retries: Number of retry attempts
-    - delay: Time (in seconds) between retries
-
-    Returns dictionary of player information from url
-    """
-
-    attempt = 0
-
-    while attempt < retries:
-
-        try:
-
-            response_var_list = [
-                'fullname', 'lastname', 'currentrank', 'peakrank', 'peakfirst', 'peaklast',
-                'dob', 'ht', 'hand', 'backhand', 'country', 'shortlist', 'careerjs', 'active', 'lastdate',
-                'twitter', 'current_dubs', 'peak_dubs', 'peakfirst_dubs', 'liverank', 'chartagg', 'photog', 'photog_credit', 'photog_link',
-                'itf_id', 'atp_id', 'dc_id', 'wiki_id', 'elo_rating', 'elo_rank',
-                'matchmx',
-            ]
-            
-            # initialize data
-            player_dict = {}
-
-            # navigate to the page
-            response = make_request(url=player_classic_url)
-            response_text = response.text
-            # soup = BeautifulSoup(response_text, 'html.parser')
-
-            for var in response_var_list:
-                try:
-                    # parse matchmx
-                    if var == 'matchmx':
-                        try:
-                            val = scrape_javascript_multiline_var(
-                                content=response_text,
-                                var=var
-                            )
-
-                            # convert to string if not null
-                            if val is not None:
-                                val = ast.literal_eval(val)
-                                val = parse_player_classic_matchmx(player_matchmx_list=val)
-                                val = json.dumps(val)
-                                # append to dict
-                                player_dict[var] = val
-
-                        except Exception as e:
-                            logging.error(f"Error scraping {var}: {e}.")
-                            
-
-                    else:
-                        val = scrape_javascript_var(
-                            content=response_text,
-                            var=var
-                        )
-
-                        # check if value exists
-                        if (val is not None) and (val != '') and (val != "") and (val != "''") and (val != '""'):
-                            # check if quotes exist in string
-                            if ('"' not in val) and ("'" not in val):
-                                val = f'"{val}"'
-                        
-                            # append to dict
-                            player_dict[var] = val
-
-                except Exception as e:
-                    logging.info(f"Error encountered when getting data for variable {var}: {e}")
-                    continue
-
-            # check if dict is empty
-            if player_dict == {}:
-                
-                logging.info(f"All values null for: {player_classic_url}")
-                
                 attempt += 1
                 
                 # retry if possible
---
+++ scripts/web/tennisabstract/players_classic/get_url_list.py
@@ -0,0 +1,109 @@
+from typing import (
+    Dict,
+    List,
+)
+from scripts.web.tennisabstract.matches.get_url_list import (
+    main as get_match_url_list,
+)
+from scripts.web.tennisabstract.players_classic.create_player_classic_url import (
+    create_player_classic_url,
+)
+from utils.python.combine_list_of_dicts import combine_list_of_dicts
+from utils.web.scrape_javascript_var import scrape_javascript_var
+from utils.web.make_request import make_request
+import ast
+import re
+
+def main():
+
+    # here is where the function can be swapped out
+    player_list = get_player_classic_url_list_from_matches()
+    return player_list
+
+def get_player_classic_url_list() -> List[Dict]:
+    """
+    Returns list of player urls from source (url)
+    """
+
+    # retrieve url page
+    player_list_url = 'https://www.tennisabstract.com/jsplayers/mwplayerlist.js'
+    response = make_request(player_list_url)
+    
+    # retrieve list-like string
+    player_list_val = scrape_javascript_var(
+        content=response.text,
+        var='playerlist'
+    )
+    # convert to list
+    player_list = ast.literal_eval(player_list_val)
+
+    # loop through each element and create url
+    player_classic_url_list = []
+    for player in player_list:
+
+        # each element in list is of format: (<gender>) <name>)
+        regex_pattern = r'(?P<gender>\((.*?)\))\s*(?P<name>.*)'
+        regex_match = re.search(regex_pattern, player)
+        gender = regex_match.group('gender').strip('()')
+        name = regex_match.group('name')
+
+        player_classic_url_dict = {}
+
+        player_classic_url_dict['player_name'] = name
+        player_classic_url_dict['player_gender'] = gender
+
+        # create url
+        player_classic_url = create_player_classic_url(
+            player_gender=gender,
+            player_name=name
+        )
+        player_classic_url_dict['player_classic_url'] = player_classic_url
+
+        player_classic_url_list.append(player_classic_url_dict)
+
+    return player_classic_url_list
+
+def get_player_classic_url_list_from_matches() -> List[Dict]:
+    """
+    Returns list of player urls using the match url list.
+    """
+
+    # get match url list
+    match_url_list = get_match_url_list()
+
+    # initialize player dicts
+    player_one_url_list = []
+    player_two_url_list = []
+
+    # loop through match data
+    for match_url_dict in match_url_list:
+
+        # create player dicts and append to player lists
+        player_one_url_dict = {
+            'player_name': match_url_dict['match_player_one'].replace('_', ' '),
+            'player_gender': match_url_dict['match_gender'],
+        }
+        player_one_url_list.append(player_one_url_dict)
+        player_two_url_dict = {
+            'player_name': match_url_dict['match_player_two'].replace('_', ' '),
+            'player_gender': match_url_dict['match_gender'],
+        }
+        player_two_url_list.append(player_two_url_dict)
+
+    # combine player lists
+    player_classic_url_list = combine_list_of_dicts(
+        player_one_url_list,
+        player_two_url_list
+    )
+
+    # loop through player list
+    for player_classic_url_dict in player_classic_url_list:
+
+        # create url
+        player_classic_url = create_player_classic_url(
+            player_gender=player_classic_url_dict['player_gender'],
+            player_name=player_classic_url_dict['player_name']
+        )
+        player_classic_url_dict['player_classic_url'] = player_classic_url
+        
+    return player_classic_url_list
